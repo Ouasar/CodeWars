91f34cd036aa822d65b3786b12201df710b31335
Kirill Molozin <Kir3d@ya.ru>
2018-05-14 12:48:40 +0300

    Remove_anchor_from_URL

---
+++ Build Tower/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Not Done
+
+namespace Build_Tower
+{
+    class Program
+    {
+        private static string block = "*";
+
+        static void Main(string[] args)
+        {
+            foreach (var O in TowerBuilder(3))
+            {
+                Console.WriteLine(O);
+            }
+        }
+
+        public static string[] TowerBuilder(int nFloors)
+        {
+            List<string> floorArray = null;
+
+            for (int i = 1; i < nFloors; i++)
+            {
+                if (nFloors - i != 0)
+                {
+                    string floor = block;
+                    floorArray.Add("");
+
+                    floor.Insert(floor.Length, "").Insert(0, "");
+                }
+            }
+
+            return new string[] { };
+        }
+    }
+}
+
+#endregion
---
+++ Remove anchor from URL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+
+namespace Remove_anchor_from_URL
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Method("www.codewars.com/katas/?page=1#about"));
+        }
+
+        public static string Method(string url)
+        {
+            if (url.Contains("#"))
+                return url.Remove(url.IndexOf('#'));
+            else
+                return url;
+        }
+    }
+}
