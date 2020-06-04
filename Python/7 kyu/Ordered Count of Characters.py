9fd04e9615476cbb93e4fcfd86996c43885d0546
Kir <Kir3d@ya.ru>
2020-06-05 01:53:55 +0300

    Ordered Count of Characters

---
+++ Python/7 kyu/Ordered Count of Characters.py
@@ -0,0 +1,24 @@
+import CodewarsLib.test_framework as test
+from collections import Counter
+
+
+# Arrange
+def ordered_count(inp):
+    return list(Counter(inp).items())
+
+
+# Act
+def run_test():
+    tests = (
+        ('ab', [('a', 1), ('b', 1)]),
+        ('abracadabra', [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]),
+        ('Code Wars', [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)])
+    )
+
+    for t in tests:
+        inp, exp = t
+        test.assert_equals(ordered_count(inp), exp)
+
+
+# Assert
+run_test()
